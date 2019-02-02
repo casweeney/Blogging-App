d893eb4a1f5f3a951b53ace4710503194c3340d7
casweeney <casweeno2000@gmail.com>
2019-02-02 14:45:26 +0100

    Editing and Deleting of Post modules added
    User can edit blog post and also delete blog posts using Laravel.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 
 @section('content')
+    <a href="/posts" class="btn btn-light btn-sm">Go Back</a>
     <h1>{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
@@ -8,5 +9,10 @@
     <hr>
     <code>Written on {{$post->created_at}}</code>
     <br>
-    <a href="/posts" class="btn btn-secondary btn-sm">Go Back</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'pull-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class'=>'btn btn-danger btn-sm'])}}
+    {!!Form::close()!!}
 @endsection
